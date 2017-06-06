d5e74365b2a746f0dcbc3262b584f7aa53bdde5a
nahami <n.ahami@live.com>
2017-06-06 13:37:52 +0000

    upd 6 juni

--- rs_1.R
+++ rs_1.R
@@ -1,13 +1,15 @@
-# Thesis: Statistical modeling of the Urban Heat Island ----
+#------------------
+# Thesis: Statistical modeling of the Urban Heat Island
 # May 2017, by Nadir Ahami
-# ...Explain script/project...
+# 
 
 # Set up environment --------------------------
 rm(list=ls())
 
 # Set working directory
 # dir <- "/usr/people/ahami/_msc_thesis/_r_thesis"
-dir <- "C:/Users/Nadir/hubiC/_Studie/MscThesis/_r_thesis"
+# dir <- "C:/Users/Nadir/hubiC/_Studie/MscThesis/_r_thesis"
+dir <- "/usr/people/ahami/_msc_thesis/_r_thesis"
 setwd(dir)
 
 # packages
@@ -18,6 +20,7 @@ library(R.utils)
 # load wow and TUD data coordinates and IDs
 idlatlon <- read.csv('data/Rdam/rdam_all', header = FALSE)
 colnames(idlatlon) <- c('Name', 'id', 'lat', 'lon', 'owner')
+rm(idlatlon)
 
 flwowrdam <- paste(dir,'/data/Rdam/WOW/',list.files('data/Rdam/WOW'), sep = '')
 fltudrdam <- paste(dir,'/data/Rdam/TUDelft/',list.files('data/Rdam/TUDelft',pattern = '.csv'), sep = '')
@@ -25,8 +28,9 @@ fltudrdam <- paste(dir,'/data/Rdam/TUDelft/',list.files('data/Rdam/TUDelft',patt
 datardam_wow <- lapply(X = flwowrdam,read.csv,sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "-")
 # datardam_tud <- lapply(X = fltudrdam,read.csv)
 
+#TUD Data
 for (e in seq_along(fltudrdam)){
-  l2keep <- 5000
+  l2keep <- 16500
   nL <- countLines(fltudrdam[e])
   data <- read.csv(fltudrdam[e], skip=nL-l2keep,stringsAsFactors = FALSE, na.strings = "NaN")
   colnames(data) <- c( 'DateTime',
@@ -64,22 +68,37 @@ for (e in seq_along(fltudrdam)){
                        'Albedo'
   )
   
-  assign(paste('tud_',data[1,2],sep = ''), data)
+  data <- as.data.frame(cbind(id = data$'Name', datetime = data$'DateTime', temp = as.double(data$'Tair_{Avg}')))
+  data$datetime <- strptime(data$datetime, format = "%Y-%m-%d %H:%M:%S")
+  assign(paste('tud_',data[1,1],sep = ''), data)
   print(sprintf('imported %i files',e))
   rm(data,l2keep,nL)
 }
 
-
+#WOW Data
 for (e in seq_along(datardam_wow)) {
   data <- as.data.frame(datardam_wow[e])
   data <- cbind(id = as.numeric(gsub("\\D", "", colnames(data)[2])),data)
   st_id = as.numeric(gsub("\\D", "", colnames(data)[3]))
+  data <- as.data.frame(cbind(id = data[,1], datetime = data[,2], temp = data[,3]))
+  data$datetime <- strptime(data$datetime, format = "%Y-%m-%dT%H:%M:%S")
   assign(paste('wow_',st_id,sep = ''),data)
   rm(data,st_id)
   # print(e)
 }
 
+#KNMI AWS 
+KNMI_AWS <- read.csv(file = 'data/Rdam/KNMI_AWS/export_915096001.csv', sep = ";", header = TRUE, stringsAsFactors = FALSE, na.strings = "-") 
+KNMI_AWS <- cbind(id = as.numeric(gsub("\\D", "", colnames(KNMI_AWS)[2])),KNMI_AWS)
+KNMI_AWS <- as.data.frame(cbind(id = KNMI_AWS[,1], datetime = KNMI_AWS[,2], temp = KNMI_AWS[,3]))
+
 rm(e,dir,fltudrdam,flwowrdam,datardam_wow)
 
 
-# save(aaa,file = 'data_tables.RData')
+# Get list of all dfs and combine into 1 df
+dfs = sapply(.GlobalEnv, is.data.frame) 
+alld <- do.call(rbind, mget(names(dfs)[dfs]))
+
+# save df 
+save(alld,file = 'obs.RData')
+
